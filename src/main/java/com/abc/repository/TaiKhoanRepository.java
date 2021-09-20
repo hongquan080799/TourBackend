8119d2ef218a8cfb2bfcf6c829c61f87c826781d
Thanh Nguyen <45066086+vanthanhnguyen99@users.noreply.github.com>
2021-09-21 00:28:48 +0700

    Delete TaiKhoanRepository.java

--- src/main/java/com/abc/repository/TaiKhoanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.abc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.abc.entity.Taikhoan;
-
-public interface TaiKhoanRepository extends JpaRepository<Taikhoan,String> {
-
-}
